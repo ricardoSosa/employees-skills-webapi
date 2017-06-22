68d2a79ab6a899607165d180ec000b042378b2bd
Rudy Omar Carrillo Méndez <RudyCarrillo@users.noreply.github.com>
2017-06-22 11:47:54 -0500

    added EMployee detail

---
+++ SkillMService/Models/Knows.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillMService.Models
+{
+    public class Knows
+    {
+        public int id { get; set; }
+        public int value { get; set; }
+    }
+}
---
+++ SkillMService/Results/EmployeeSkillResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SkillMService.Models;
+
+namespace SkillMService.Results
+{
+    public class EmployeeSkillResult
+    {
+        public EmployeeSkillResult() { }
+
+        public EmployeeSkillResult( string id, 
+                                    string name, 
+                                    int location,
+                                    List<Skill> jrskills,
+                                    List<Skill> intskills,
+                                    List<Skill> srskills,
+                                    List<Skill> ldskills
+                                        )
+        {
+            this.ID = id;
+            this.NAME = name;
+            this.JrSkills = jrskills;
+            this.SrSkills = srskills;
+            this.LdSkills = ldskills;
+        }
+
+        public string ID { get; set; }
+        public string NAME { get; set; }
+        public int LOCATION { get; set; }
+        public List<Skill> JrSkills { get; set; }
+        public List<Skill> IntSkills { get; set; }
+        public List<Skill> SrSkills { get; set; }
+        public List<Skill> LdSkills { get; set; }
+
+
+        public byte Status { get; set; }
+    }
+}
